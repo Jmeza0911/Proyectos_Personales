6d1e70e29cced0822ebab72e41b7f36d7c12f951
Jesus Meza <jmeza@consware.com.co>
2023-04-12 20:38:29 -0800

    Se agregan estilos y componentes

--- Pokemon_API/src/app/app.component.ts
+++ Pokemon_API/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../assets/Scss/app.scss']
 })
 export class AppComponent {
   title = 'Pokemon_API';
--- Pokemon_API/src/app/app.module.ts
+++ Pokemon_API/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './Pokemon/Components/navbar/navbar.component';
+import { SidebarComponent } from './Pokemon/Components/sidebar/sidebar.component';
+import { TarjetaPokemonComponent } from './Pokemon/Components/tarjeta-pokemon/tarjeta-pokemon.component';
+import { ErrorPageComponent } from './Pokemon/Components/error-page/error-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    TarjetaPokemonComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
